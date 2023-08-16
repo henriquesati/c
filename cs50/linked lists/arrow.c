6e6b44146c0674868b2c472fad3617cec85f8291
pedrogatinhos <pedrohenriquebtcc@gmail.com>
2023-08-16 16:16:14 -0300

    nova estrutura

--- cs50/linked lists/arrow.c
+++
@@ -1,7 +0,0 @@
-//whenever deferencing to an adress and then using dot notation to go inside the structures that
-//is being pointed lie:
- (*tmp).item = 1;
-
- tmp -> item = 1;
-
- //gather the structure adress, and them goes inside the part
